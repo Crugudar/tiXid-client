b3df9f5ff4eedd6cbe0299663ee937d47de5efd6
lemonade <adriamartin18@gmail.com>
2020-11-24 12:40:51 +0100

    css

--- src/pages/Enter.js
+++ src/pages/Enter.js
@@ -3,40 +3,52 @@ import { withAuth } from "../lib/AuthProvider";
 import Navbar from "../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
 import "../pages/login&signup.css";
+import classes from '../components/BackgroundVideo.module.css'
 
 
 
 function Enter(props) {
+  const videoSource = "https://res.cloudinary.com/lemonade292/video/upload/v1606155635/videos%20tixid/VIDEO_Dixit_Boxart_seafhb.mp4"
   return (
     <div>
       {!props.isLoggedin ? (
             <>
          
         <div className=" authform">
-        <h1>Bienvenido a tiXid</h1>  
-          <div className="enterForm">
-            <>
-             
-              <Link className="link" to="/login">
-            Login
+        
+          <div className="enterForm2">
+            
+            <h2>Welcome to Dixit!</h2> 
+              <Link className="loginlink" to="/login">
+            Log In!
               </Link>
-              <p className="">
-                ¿Aún no estás registrado? <Link className="link" to="/signup"> Registrate </Link>
+              <p className="signuptext">
+                Don't have an account? <Link className="signuplink" to="/signup"> Sign Up! </Link>
               </p>
-            </>
+            
           </div>
         </div>
         </>
       ) : (
         <>
           <Navbar />
-          <p>BIENVENIDO {props.user.username && props.user.username}.</p>
+          <div className={classes.Container} >
+            <video autoPlay="autoplay" loop="loop" muted className={classes.Video} >
+                <source src={videoSource} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+
+            <div className={classes.Content}>
+                <div className={classes.SubContent} >
+                <p>Welcome to Dixit {props.user.username && props.user.username}.</p>
           <br />
-          <>
+         
             <Link to="/game">
-              <button>Jugar</button>
-            </Link>
-          </>
+              <button>Let's Play!</button>
+            </Link> 
+                </div>
+            </div>
+        </div>
         </>
       )}
     </div>
