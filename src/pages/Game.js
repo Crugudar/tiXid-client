1e230ced3807422dab964641875f887619c1b6f7
Claudia D <claudia.do.gallego@gmail.com>
2020-11-24 21:29:25 +0100

    casa nuria

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,9 +2,9 @@ import React, {Component} from "react";
 import { Link } from "react-router-dom";
 import io from 'socket.io-client';
 import ChatTable from '../components/ChatTable'
-import GameTable from '../components/GameTable';
-import '../components/Hand.css'
+import '../pages/Game.css'
 import auth from '../lib/auth-service'
+import Points from '../components/Points'
 
 import {withAuth} from '../lib/AuthProvider'
 
@@ -17,12 +17,12 @@ import {withAuth} from '../lib/AuthProvider'
 class Game extends Component {
 
   constructor(props) {
-    
+    console.log(props)
     super(props)
     this.state = {
-      name: null,
-      messages: [],
-      newMessage: '',
+        name: null,
+        messages: [],
+        newMessage: '',
         user:this.props.user,
         cards:[],
         playerhands:{
@@ -32,10 +32,12 @@ class Game extends Component {
             player4:[],  
         },
         
-    yourhand:[],
-    selected:[],
-    info:[],
-    currentplayer:0,
+        yourhand:[],
+        selected:[],
+        info:[],
+        currentplayer:0,
+        theChosenOne:"",
+        players:[],
        
     }
 }
@@ -47,9 +49,9 @@ class Game extends Component {
     
     
        console.log('Hola');
-        socket.emit('hola', this.props.user) 
+        socket.emit('hola',this.props.user) 
         socket.on('welcome',async (data)=>{
-        console.log(`you are in room ${data.room}, and are player ${data.player}`)
+        console.log(`Hello ${data.data} you are in room ${data.room}, and are player ${data.player}`)
          this.setState({
             currentplayer:data.player
         })
@@ -60,6 +62,13 @@ class Game extends Component {
         console.log(data)
       })
 
+      socket.on('players', (players)=>{
+          console.log('hola', players)
+          this.setState({
+              players:players,
+          })
+      })
+
       //recibe los mensajes del back
       socket.on('chat', message => {
         message.key = JSON.stringify(message)
@@ -67,26 +76,17 @@ class Game extends Component {
         this.setState({
         messages:[...this.state.messages, message]
         })  })
- 
+        
+        socket.on('selectedcards', (data)=>{
+            console.log('cartitaaaaaaaaaaaaaaaaas',data)    
+               this.setState({
+                selected:data,
+            }) 
+            console.log('estaditoooooooooooooooo',this.state.selected)              
+     })
 
 } 
-    // let validator=false;
-    // if(validator===false){
-    //     socket.emit('hola', this.props.user) 
-    //  socket.on('welcome',(data)=>{
-    //   console.log(`you are in room ${data.room}, and are player ${data.player}`)
-    //   this.setState({
-    //       currentplayer:data.player
-    //   })
-
-    //   console.log(this.state.currentplayer)
-    // })
-    //   socket.on('otherusers',(data)=>{
-    //     console.log(data)
-    //   })
-    // validator=true
-    // }
-
+   
 
 
 
@@ -192,43 +192,34 @@ class Game extends Component {
     }    
     
      selectCard(e){
-        const {name}=e.target
+        const {name, index}=e.target
+
+        this.setState({
+            theChosenOne:{index,name},
+        })
 
         console.log(name)
         
-        socket.emit('cardselected', name);
+         
+    }
 
-        socket.on('selectedcards', (data)=>{
-                console.log('cartitaaaaaaaaaaaaaaaaas',data)    
-                   this.setState({
-                    selected:data,
-                }) 
-                console.log('estaditoooooooooooooooo',this.state.selected)              
-         })
-        
+    sumbitTheChosenOne(){
+        let newHand=[...this.state.yourhand]; 
+        socket.emit('cardselected',this.state.theChosenOne.name); 
+       
+        newHand.splice(this.state.theChosenOne.index,1)
+
+        this.setState({
+           yourhand:newHand,
+       })
+
     }
 
  
 
   render(){
     
-    // if(this.state.currentplayer===0){
-    //     console.log('Hola');
-    //  socket.emit('hola', this.props.user) 
-    //   socket.on('welcome',(data)=>{
-    //    console.log(`you are in room ${data.room}, and are player ${data.player}`)
-    //    this.setState({
-    //        currentplayer:data.player
-    //    })
- 
-    //    console.log(this.state.currentplayer)
-    //  })
-    //    socket.on('otherusers',(data)=>{
-    //      console.log(data)
-    //    })
   
-    //  }
-
 
     return (
     <>
@@ -238,18 +229,18 @@ class Game extends Component {
       <div>
           {this.state.info}
       </div>
-        <div>
+        <div className="player">
             {this.state.selected.length? <>
-                <div>
+                <div className="card">
                     <div><img src={this.state.selected[0]} alt=""/></div>
                 </div>
-                <div>
+                <div className="card">
                     <div><img src={this.state.selected[1]} alt=""/></div>
                 </div>
-                <div>
+                <div className="card">
                     <div><img src={this.state.selected[2]} alt=""/></div>
                 </div>
-                <div>
+                <div className="card">
                     <div><img src={this.state.selected[3]} alt=""/></div>
                 </div>
             </>:<p>Wait for it</p>}
@@ -263,27 +254,27 @@ class Game extends Component {
       
       <div className="player">
 
-        <h1>Player {this.state.currentplayer}</h1>
+        {/* <h1>Player {this.state.currentplayer}</h1> */}
        
             <div className="card">
                 <img src={this.state.yourhand[0].url} alt=""/>
-                <button name={this.state.yourhand[0].url} onClick={(e)=>this.selectCard(e)}>Select</button>
+                <button name={this.state.yourhand[0].url} index={0} onClick={(e)=>this.selectCard(e)}>Select</button>
             </div>
         <div className="card">
             <img src={this.state.yourhand[1].url} alt=""/>
-            <button name={this.state.yourhand[1].url} onClick={(e)=>this.selectCard(e)}>Select</button>
+            <button name={this.state.yourhand[1].url} index={1} onClick={(e)=>this.selectCard(e)}>Select</button>
         </div>
         <div className="card">
             <img src={this.state.yourhand[2].url} alt=""/>
-            <button name={this.state.yourhand[2].url} onClick={(e)=>this.selectCard(e)}>Select</button>
+            <button name={this.state.yourhand[2].url} index={2} onClick={(e)=>this.selectCard(e)}>Select</button>
         </div>
         <div className="card">
             <img src={this.state.yourhand[3].url} alt=""/>
-            <button name={this.state.yourhand[3].url} onClick={(e)=>this.selectCard(e)}>Select</button>
+            <button name={this.state.yourhand[3].url} index={3} onClick={(e)=>this.selectCard(e)}>Select</button>
         </div>
        
       </div>
-     
+        <button onClick={()=>this.sumbitTheChosenOne()}>Enviar</button>
       </div>
       
       </>):<p>Loading</p>}
@@ -308,7 +299,7 @@ class Game extends Component {
 
                 </form>
                 <ChatTable messages={this.state.messages} user={this.state.user}/>
-
+                <Points players={this.state.players}/>
                 </div> 
         </div>
       </div>
